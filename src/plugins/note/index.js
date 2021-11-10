227a8592262663940355c3cce79e204112fb02aa
ixCiel <ixciel@ixciel.com>
2021-11-10 10:14:30 +0800

    add note

---
+++ src/plugins/note/index.js
@@ -0,0 +1,219 @@
+/* global alias, command */
+/* eslint no-undef: "error" */
+
+//import { hasAuth, sendPrompt } from "../../utils/auth.js";
+import { hasEntrance } from "../../utils/config.js";
+import { basePromise } from "../../utils/detail.js";
+import { getID } from "../../utils/id.js";
+import { notePromise, signInfoPromise, rewardsPromise, signInPromise, ledgerPromise, setUserCookie } from "./noteDetail.js";
+
+function getTime(s, offset) {
+    if (s + offset < 0)
+        return [0, 0, 0, 0];
+    const sec = parseInt(s + offset);
+    const min = parseInt(sec / 60);
+    const hour = parseInt(min / 60);
+    const day = parseInt(hour / 24);
+    return [day, hour % 24, min % 60, sec % 60];
+}
+
+async function Plugin(Message) {
+    const bot = Message.bot;
+    const msg = Message.text;
+    const userID = Message.uid;
+    const type = Message.type;
+    const sendID = Message.sid;
+    const dbInfo = await getID(msg, userID); // 米游社 ID
+    let uid, data, region, baseTime;
+
+    //if (!(await hasAuth(userID, "query")) || !(await hasAuth(sendID, "query"))) {
+    //  await sendPrompt(sendID, userID, "查询游戏内信息", type, bot);
+    //  return;
+    //}
+
+    if ("string" === typeof dbInfo) {
+        await bot.say(sendID, dbInfo, type, userID);
+        return;
+    }
+
+    try {
+        const baseInfo = await basePromise(dbInfo, userID, bot);
+        uid = baseInfo[0];
+        region = baseInfo[1];
+        if (hasEntrance(msg, "note", "set_user_cookie")) {
+            let cookie = msg.slice(9);
+            cookie = cookie.replace(new RegExp("'", "gm"), "");
+            if (cookie.indexOf("cookie_token") == -1 || cookie.indexOf("account_id") == -1) {
+                await bot.say(sendID, `未找到登录信息！请登录并进入米哈游通行证页面，再次尝试获取Cookie。`, type, userID);
+                return;
+            }
+            await setUserCookie(uid, cookie, bot);
+            await bot.say(sendID, `已设置cookie`, type, userID);
+            return;
+        } else if (hasEntrance(msg, "note", "sign_in")) {
+            let signInfo = await signInfoPromise(uid, region, userID, bot);
+            if (!signInfo) {
+                await bot.say(
+                    sendID,
+                    `[CQ:at,qq=${userID}] 获取签到信息失败`,
+                    type,
+                    userID
+                );
+                return;
+            }
+            if (signInfo.is_sign) {
+                await bot.say(
+                    sendID,
+                    `[CQ:at,qq=${userID}] 今日已签到,本月累计签到${signInfo.total_sign_day}天`,
+                    type,
+                    userID
+                );
+                return;
+            }
+            if (signInfo.first_bind) {
+                await bot.say(
+                    sendID,
+                    `[CQ:at,qq=${userID}] 请先手动签到一次`,
+                    type,
+                    userID
+                );
+                return;
+            }
+            let sign = await signInPromise(uid, region, userID, bot);
+            if (!sign) {
+                await bot.say(
+                    sendID,
+                    `[CQ:at,qq=${userID}] 签到失败`,
+                    type,
+                    userID
+                );
+                return;
+            }
+            data = await rewardsPromise(uid, region, userID, bot);
+            if (!data) {
+                await bot.say(
+                    sendID,
+                    `[CQ:at,qq=${userID}] 签到失败`,
+                    type,
+                    userID
+                );
+                return;
+            }
+            await bot.say(
+                sendID,
+                `[CQ:at,qq=${userID}]
+${data.month}月累计签到：${++signInfo.total_sign_day}天
+今日奖励：${data.awards[signInfo.total_sign_day - 1].name} * ${data.awards[signInfo.total_sign_day - 1].cnt}`,
+                type,
+                userID
+            );
+            return;
+        } else if (hasEntrance(msg, "note", "ledger") || hasEntrance(msg, "note", "lastledger") || hasEntrance(msg, "note", "lastlastledger")) {
+            data = await ledgerPromise(uid, region, userID, bot);
+            if (!data) {
+                await bot.say(
+                    sendID,
+                    `[CQ:at,qq=${userID}] 获取札记失败`,
+                    type,
+                    userID
+                );
+                return;
+            }
+            if (hasEntrance(msg, "note", "lastledger"))
+                data = await ledgerPromise(uid, region, userID, bot, data.data_month == 1 ? 12 : data.data_month - 1);
+            else if (hasEntrance(msg, "note", "lastlastledger"))
+                data = await ledgerPromise(uid, region, userID, bot, data.data_month > 2 ? data.data_month - 2 : 10 + data.data_month);
+            if (!data) {
+                await bot.say(
+                    sendID,
+                    `[CQ:at,qq=${userID}] 获取札记失败`,
+                    type,
+                    userID
+                );
+                return;
+            }
+            if (hasEntrance(msg, "note", "ledger"))
+                await bot.say(
+                    sendID,
+                    `[CQ:at,qq=${userID}]
+旅行者${data.data_month}月札记
+当月共获取：
+原石：${data.month_data.current_primogems}
+摩拉：${data.month_data.current_mora}
+旅行者今日已获取${data.day_data.current_primogems}原石，${data.day_data.current_mora}摩拉，明天也要好好努力哦？`,
+                    type,
+                    userID
+                );
+            else
+                await bot.say(
+                    sendID,
+                    `[CQ:at,qq=${userID}]
+旅行者${data.data_month}月札记
+当月共获取：
+原石：${data.month_data.current_primogems}
+摩拉：${data.month_data.current_mora}
+原石收入${data.month_data.primogems_rate == 0 ? "跟上个月差不多" : `比上个月${data.month_data.primogems_rate > 0 ? `增加${data.month_data.primogems_rate}` : `减少${-data.month_data.primogems_rate}`}%`},
+摩拉收入${data.month_data.mora_rate == 0 ? "跟上个月差不多" : `比上个月${data.month_data.mora_rate > 0 ? `增加${data.month_data.mora_rate}` : `减少${-data.month_data.mora_rate}`}%`}。`,
+                    type,
+                    userID
+                );
+            return;
+        }
+        const noteInfo = await notePromise(uid, region, userID, bot);
+        data = noteInfo[1];
+        baseTime = noteInfo[0];
+        if (!data) {
+            await bot.say(
+                sendID,
+                `[CQ:at,qq=${userID}] 获取实时便笺失败`,
+                type,
+                userID
+            );
+            return;
+        }
+    } catch (e) {
+        // 抛出空串则使用缓存
+        if ("" !== e) {
+            await bot.say(sendID, `[CQ:at,qq=${userID}] ${e}`, type, userID);
+            return;
+        }
+    }
+    //if (hasEntrance(msg, "note", "get_note_pic")) {
+    //    await render({ uid, data }, "genshin-note", sendID, type, userID, bot);
+    //    return;
+    //}
+    const nowTime = new Date().valueOf();
+    let message = `[CQ:at,qq=${userID}]
+树脂${data.current_resin}/${data.max_resin} 委托${data.finished_task_num}/${data.total_task_num} 派遣${data.current_expedition_num}/${data.max_expedition_num}`;
+    let [day, hour, min, sec] = getTime(parseInt(data.resin_recovery_time), (baseTime - nowTime) / 1000);
+    message += `
+树脂回满时间：${hour}时${min}分${sec}秒`;
+    message += `
+[每日委托]奖励${data.is_extra_task_reward_received ? "已领取" : "未领取"}`;
+    message += `
+本周剩余消耗减半次数${data.remain_resin_discount_num}/${data.resin_discount_num_limit}`;
+    let num = 1;
+    for (var expedition of data.expeditions) {
+        if (expedition)
+            if (expedition.status == "Ongoing") {
+                [day, hour, min, sec] = getTime(parseInt(expedition.remained_time), (baseTime - nowTime) / 1000);
+                message += `
+派遣${num}：${hour}时${min}分${sec}秒`;
+            } else if (expedition.status == "Finished") {
+                message += `
+派遣${num}：已完成`;
+            }
+        num++;
+    }
+    await bot.say(sendID, message, type, userID);
+}
+
+async function Wrapper(Message, bot) {
+    try {
+        await Plugin(Message, bot);
+    } catch (e) {
+        bot.logger.error(e);
+    }
+}
+
+export { Wrapper as run };
---
+++ src/plugins/note/noteDetail.js
@@ -0,0 +1,241 @@
+
+import moment from "moment-timezone";
+import lodash from "lodash";
+import db from "../../utils/database.js";
+import fs from "fs";
+import path from "path";
+import fetch from "node-fetch";
+import { getDS } from "../../utils/ds.js";
+import md5 from "md5";
+import { randomString } from "../../utils/tools.js";
+import { v3 as uuidv3 } from 'uuid';
+
+const __API = {
+    FETCH_ROLE_DAILY_NOTE: "https://api-takumi.mihoyo.com/game_record/app/genshin/api/dailyNote",
+    REFERER_URL: "https://webstatic.mihoyo.com/bbs/event/signin-ys/index.html?bbs_auth_required=true&act_id=e202009291139501&utm_source=bbs&utm_medium=mys&utm_campaign=icon",
+    SIGN_INFO_URL: "https://api-takumi.mihoyo.com/event/bbs_sign_reward/info",
+    SIGN_URL: "https://api-takumi.mihoyo.com/event/bbs_sign_reward/sign",
+    REWARD_URL: "https://api-takumi.mihoyo.com/event/bbs_sign_reward/home",
+    LEDGER_URL: "https://hk4e-api.mihoyo.com/event/ys_ledger/monthInfo",
+};
+const HEADERS = {
+    "User-Agent":
+        "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) miHoYoBBS/2.11.1",
+    Referer: "https://webstatic.mihoyo.com/",
+    "x-rpc-app_version": "2.11.1",
+    "x-rpc-client_type": 5,
+    DS: "",
+    Cookie: "",
+};
+
+async function getUserCookie(user, bot) {
+    if (!(await db.includes("note", "cookie", "user", user))) {
+        const initData = { user, cookie: "" };
+        await db.push("note", "cookie", initData);
+    }
+    let { cookie } = await db.get("note", "cookie", { user });
+    return cookie;
+}
+
+async function setUserCookie(user, userCookie, bot) {
+    if (!(await db.includes("note", "cookie", "user", user))) {
+        const initData = { user, cookie: "" };
+        await db.push("note", "cookie", initData);
+    }
+    await db.update("note", "cookie", { user }, { cookie: userCookie });
+}
+
+function getDailyNote(role_id, server, cookie) {
+    const query = { role_id, server };
+
+    return fetch(`${__API.FETCH_ROLE_DAILY_NOTE}?${new URLSearchParams(query)}`, {
+        method: "GET",
+        qs: query,
+        headers: { ...HEADERS, DS: getDS(query), Cookie: cookie },
+    }).then((res) => res.json());
+}
+
+function getSignInfo(role_id, server, cookie) {
+    const query = { region: server, act_id: "e202009291139501", uid: role_id, };
+
+    return fetch(`${__API.SIGN_INFO_URL}?${new URLSearchParams(query)}`, {
+        method: "GET",
+        qs: query,
+        headers: { ...HEADERS, DS: getDS(query), Cookie: cookie, Referer: __API.REFERER_URL },
+    }).then((res) => res.json());
+}
+
+function getRewardsInfo(cookie) {
+    const query = { act_id: "e202009291139501", };
+
+    return fetch(`${__API.REWARD_URL}?${new URLSearchParams(query)}`, {
+        method: "GET",
+        qs: query,
+        headers: { ...HEADERS, DS: getDS(query), Cookie: cookie, Referer: __API.REFERER_URL },
+    }).then((res) => res.json());
+}
+
+function mysSignIn(role_id, server, cookie) {
+    const body = { act_id: "e202009291139501", region: server, uid: role_id, };
+    const n = "h8w582wxwgqvahcdkpvdhbh2w9casgfl";
+    const i = (Date.now() / 1000) | 0;
+    const r = randomString(6);
+    const c = md5(`salt=${n}&t=${i}&r=${r}`);
+
+    return fetch(__API.SIGN_URL, {
+        method: "POST",
+        json: true,
+        body: JSON.stringify(body),
+        headers: {
+            ...HEADERS,
+            DS: `${i},${r},${c}`,
+            Cookie: cookie,
+            Referer: __API.REFERER_URL,
+            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) miHoYoBBS/2.3.0",
+            "x-rpc-app_version": "2.3.0",
+            "x-rpc-client_type": 5,
+            "x-rpc-device_id": uuidv3(cookie, uuidv3.URL).replace("-", ""),
+        },
+    }).then((res) => res.json());
+}
+
+function getLedger(bind_uid, bind_region, cookie, month = 0) {
+    const query = { month: month, bind_uid: bind_uid, bind_region: bind_region, bbs_presentation_style: "fullscreen", bbs_auth_required: true, mys_source: "GameRecord" };
+
+    return fetch(`${__API.LEDGER_URL}?${new URLSearchParams(query)}`, {
+        method: "GET",
+        qs: query,
+        headers: { ...HEADERS, DS: getDS(query), Cookie: cookie, Referer: "https://webstatic.mihoyo.com/ys/event/e20200709ysjournal/index.html?bbs_presentation_style=fullscreen&bbs_auth_required=true&mys_source=GameRecord" },
+    }).then((res) => res.json());
+}
+
+async function notePromise(uid, server, userID, bot) {
+    //await userInitialize(userID, uid, "", -1);
+    //db.update("character", "user", { userID }, { uid });
+
+    //const nowTime = new Date().valueOf();
+    //const { time: lastTime } = db.get("time", "user", { note: uid }) || {};
+    //const { data: dbData } = db.get("note", "user", { uid }) || {};
+
+    // 尝试使用缓存
+    //if (dbData) {
+    //    if (
+    //        lastTime &&
+    //        nowTime - lastTime < config.cacheAbyEffectTime * 60 * 60 * 1000
+    //    ) {
+    //        bot.logger.debug(
+    //            `缓存：使用 ${uid} 在 ${config.cacheAbyEffectTime} 小时内的实时便笺。`
+    //        );
+    //        return [lastTime, dbData];
+    //    }
+    //}
+
+    const cookie = await getUserCookie(uid, bot);
+    if (!cookie)
+        return Promise.reject(`未设置私人cookie`);
+
+    const { retcode, message, data } = await getDailyNote(
+        uid,
+        server,
+        cookie
+    );
+
+    if (retcode !== 0) {
+        return Promise.reject(`米游社接口报错: ${message}`);
+    }
+
+    if (!db.includes("note", "user", "uid", uid)) {
+        const initData = { uid, data: [] };
+        db.push("note", "user", initData);
+    }
+
+    db.update("note", "user", { uid }, { data });
+    //db.update("time", "user", { note: uid }, { time: nowTime });
+    //bot.logger.debug(
+    //    `缓存：新增 ${uid} 的实时便笺，缓存 ${config.cacheAbyEffectTime} 小时。`
+    //);
+
+    return [nowTime, data];
+}
+
+
+async function signInfoPromise(uid, server, userID, bot) {
+    const cookie = await getUserCookie(uid, bot);
+    if (!cookie)
+        return Promise.reject(`未设置私人cookie`);
+    bot.logger.debug(
+        `signInfo ${uid} ${server} ${cookie}`
+    );
+    const { retcode, message, data } = await getSignInfo(
+        uid,
+        server,
+        cookie
+    );
+
+    if (retcode !== 0) {
+        return Promise.reject(`米游社接口报错: ${message}`);
+    }
+
+    return data;
+}
+
+async function rewardsPromise(uid, server, userID, bot) {
+    const cookie = await getUserCookie(uid, bot);
+    if (!cookie)
+        return Promise.reject(`未设置私人cookie`);
+    bot.logger.debug(
+        `rewards ${uid} ${server} ${cookie}`
+    );
+    const { retcode, message, data } = await getRewardsInfo(
+        cookie
+    );
+
+    if (retcode !== 0) {
+        return Promise.reject(`米游社接口报错: ${message}`);
+    }
+
+    return data;
+}
+
+async function signInPromise(uid, server, userID, bot) {
+    const cookie = await getUserCookie(uid, bot);
+    if (!cookie)
+        return Promise.reject(`未设置私人cookie`);
+    bot.logger.debug(
+        `signIn ${uid} ${server} ${cookie}`
+    );
+    const { retcode, message, data } = await mysSignIn(
+        uid,
+        server,
+        cookie
+    );
+
+    if (retcode !== 0) {
+        return Promise.reject(`米游社接口报错: ${message}`);
+    }
+
+    return data;
+}
+
+async function ledgerPromise(uid, server, userID, bot, month = 0) {
+    const cookie = await getUserCookie(uid, bot);
+    if (!cookie)
+        return Promise.reject(`未设置私人cookie`);
+    bot.logger.debug(
+        `ledger ${uid} ${server} ${cookie}`
+    );
+    const { retcode, message, data } = await getLedger(
+        uid,
+        server,
+        cookie,
+        month
+    );
+
+    if (retcode !== 0) {
+        return Promise.reject(`米游社接口报错: ${message}`);
+    }
+
+    return data;
+}
+
+export { notePromise, signInfoPromise, rewardsPromise, signInPromise, ledgerPromise, setUserCookie };
