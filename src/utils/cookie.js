1e1c6c1365dbc91915ff17b97f42e76094a8f1e3
ixCiel <ixciel@ixciel.com>
2021-11-25 17:16:32 +0800

    fix cookie

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -62,17 +62,20 @@ function getEffectiveCookie(uid, s, use_cookie) {
   return cookie;
 }
 
-function getUCookie(user) {
+function getUCookie(user,bot) {
     if (!(db.includes("note", "cookie", "user", user))) {
         const initData = { user, cookie: "" };
         db.push("note", "cookie", initData);
     }
     let { cookie } = db.get("note", "cookie", { user });
+    bot.logger.debug(
+        `UCookie ${user} ${cookie}`
+    );
     return cookie;
 }
 
 function getCookie(uid, use_cookie, bot) {
-  let userCookie = getUCookie(uid);
+  let userCookie = getUCookie(uid,bot);
   if (userCookie != undefined)
     return userCookie;
 
